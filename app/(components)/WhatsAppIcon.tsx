ccf790af2a47e06c5ad6455b79f91af59e7ab4b7
Yash veer <yashmoriya0@gmail.com>
2026-02-06 18:53:37 +0530

    testing done

--- app/(components)/WhatsAppIcon.tsx
+++ app/(components)/WhatsAppIcon.tsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const WhatsAppIcon = () => {
   return (
     <Link
-      href="https://wa.me/919876543210"
+      href="https://wa.me/919217070575"
       target="_blank"
       className="fixed bottom-6 mb-15 right-6 z-50 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg transition-all duration-300"
     >
