e13abe36aa16aef14cf693c7108e6ef153abf3cb
Yash veer <yashmoriya0@gmail.com>
2026-02-10 10:18:51 +0530

    wst btn size fxied

--- app/(components)/WhatsAppIcon.tsx
+++ app/(components)/WhatsAppIcon.tsx
@@ -8,7 +8,7 @@ const WhatsAppIcon = () => {
       target="_blank"
       className="fixed bottom-6 mb-15 right-6 z-50 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg transition-all duration-300"
     >
-      <FaWhatsapp size={28} />
+      <FaWhatsapp size={18} />
     </Link>
   );
 };
